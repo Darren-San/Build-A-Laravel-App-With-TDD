045db1873b5564547ef54f64d9936a3427c0f590
Darren-San <jpineda.nullglass@gmail.com>
2021-02-23 12:45:22 +0100

    Scoping Projects

--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -11,6 +11,29 @@ class ProjectsTest extends TestCase
   use WithFaker, RefreshDatabase;
 
   /** @test */
+  public function guests_cannot_create_projects()
+  {
+    $attributes = factory('App\Project')->raw();
+
+    $this->post('/projects', $attributes)->assertRedirect('login');
+  }
+
+  /** @test */
+  public function guests_cannot_view_projects()
+  {
+
+    $this->get('/projects')->assertRedirect('login');
+  }
+
+  /** @test */
+  public function guests_cannot_view_a_single_project()
+  {
+    $project = factory('App\Project')->create();
+
+    $this->get($project->path())->assertRedirect('login');
+  }
+
+  /** @test */
   public function a_user_can_create_a_project()
   {
     $this->withoutExceptionHandling();
@@ -30,15 +53,27 @@ class ProjectsTest extends TestCase
   }
 
   /** @test */
-  public function a_user_can_view_a_project()
+  public function a_user_can_view_their_project()
   {
+    $this->be(factory('App\User')->create());
+
     $this->withoutExceptionHandling();
 
-    $project = factory('App\Project')->create();
+    $project = factory('App\Project')->create(['owner_id' => auth()->id()]);
 
     $this->get('/projects/' . $project->id)
     ->assertSee($project->title)
     ->assertSee($project->description);
+  }
+
+  /** @test */
+  public function an_authenticated_user_cannot_view_the_projects_of_others()
+  {
+    $this->be(factory('App\User')->create());
+
+    $project = factory('App\Project')->create();
+
+    $this->get($project->path())->assertStatus(403);
   }
 
   /** @test */
@@ -59,13 +94,5 @@ class ProjectsTest extends TestCase
     $attributes = factory('App\Project')->raw(['description' => '']);
 
     $this->post('/projects', [])->assertSessionHasErrors('description');
-  }
-
-  /** @test */
-  public function only_authenticated_sers_can_create_projects()
-  {
-    $attributes = factory('App\Project')->raw();
-
-    $this->post('/projects', $attributes)->assertRedirect('login');
   }
 }
