5389dfb6cc10de743d11fbd990e2a37c7fe21ecb
Darren-San <jpineda.nullglass@gmail.com>
2021-03-15 14:10:41 +0100

    21 - Project Activity Feeds: Part 2

--- app/Task.php
+++ app/Task.php
@@ -6,17 +6,37 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    protected $guarded = [];
+  protected $guarded = [];
 
-    protected $touches = ['project'];
+  protected $touches = ['project'];
 
-    public function project()
-    {
-      return $this->belongsTo(Project::class);
-    }
+  protected $casts = [
+    'completed' => 'boolean'
+  ];
 
-    public function path()
-    {
-      return "/projects/{$this->project->id}/tasks/{$this->id}";
-    }
+  protected static function boot()
+  {
+    parent::boot();
+
+    static::created(function ($task) {
+      $task->project->recordActivity('created_task');
+    });
+  }
+
+  public function complete()
+  {
+    $this->update(['completed' => true]);
+
+    $this->project->recordActivity('completed_task');
+  }
+
+  public function project()
+  {
+    return $this->belongsTo(Project::class);
+  }
+
+  public function path()
+  {
+    return "/projects/{$this->project->id}/tasks/{$this->id}";
+  }
 }
