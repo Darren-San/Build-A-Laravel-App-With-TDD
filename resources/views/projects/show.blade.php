ead5e5dc6c9f6bff5e9cd838c18f4a660de0dabe
Darren-San <jpineda.nullglass@gmail.com>
2021-03-18 11:48:18 +0100

    24 - Rendering Activity With Polymorphism

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,14 +1,12 @@
 @extends ('layouts.app')
 
 @section('content')
-
   <header class="flex items-center mb-3 pb-4">
     <div class="flex justify-between items-end w-full">
       <p  class="text-gray-400 text-sm font-normal">
         <a href="/projects" class="text-gray-400 text-sm font-normal no-underline">My projects</a>
         / {{ $project->title  }}
       </p>
-
       <a href="{{ $project->path().'/edit' }}" class="button">Edit Project</a>
     </div>
   </header>
@@ -37,12 +35,10 @@
           <div class="card mb-3">
             <form action="{{ $project->path() . '/tasks' }}" method="POST">
               @csrf
-
               <input placeholder="Add a new task..." class="w-full" name="body">
             </form>
           </div>
         </div>
-
         <div>
           <h2  class="text-lg text-gray-400 font-normal mb-3">General Notes</h2>
 
@@ -50,23 +46,23 @@
           <form method="POST" action="{{ $project->path() }}">
             @csrf
             @method('PATCH')
-
             <textarea
-              name="notes"
-              class="card w-full mb-4"
-              style="min-height:200px"
-              placeholder="Anything special that you want to make a note of?"
-              >{{ $project->notes }}</textarea>
-
-              <button type="submit" class="button">Save</button>
+            name="notes"
+            class="card w-full mb-4"
+            style="min-height:200px"
+            placeholder="Anything special that you want to make a note of?"
+            >{{ $project->notes }}</textarea>
+            <button type="submit" class="button">Save</button>
           </form>
-        </div>
 
+        </div>
       </div>
-      <div class="lg:w-1/4 px-3">
+
+      <div class="lg:w-1/4 px-3 lg:py-8">
         @include ('projects.card')
+        @include ('projects.activity.card')
       </div>
+
     </div>
   </main>
-
 @endsection
