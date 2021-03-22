52f131059b10ed8f11eb37cc1a6c4c21f6719c23
Darren-San <jpineda.nullglass@gmail.com>
2021-03-22 11:58:25 +0100

    26 - Recording Model Changes

--- app/Observers/ProjectObserver.php
+++ app/Observers/ProjectObserver.php
@@ -7,23 +7,16 @@ use App\Project;
 
 class ProjectObserver
 {
-  /**
-  * Handle the project "created" event.
-  *
-  * @param  \App\Project $project
-  * @return void
-  */
   public function created(Project $project)
   {
     $project->recordActivity('created');
   }
 
-  /**
-  * Handle the project "updated" event.
-  *
-  * @param  \App\Project $project
-  * @return void
-  */
+  public function updating(Project $project)
+  {
+    $project->old = $project->getOriginal();
+  }
+
   public function updated(Project $project)
   {
     $project->recordActivity('updated');
