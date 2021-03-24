8d64230fcdd241225a3e0ee63a8b74f952328c01
Darren-San <jpineda.nullglass@gmail.com>
2021-03-24 10:19:25 +0100

    minor styling tweaks

--- resources/views/projects/card.blade.php
+++ resources/views/projects/card.blade.php
@@ -2,12 +2,12 @@
   <h3 class="font-normal text-xl py-4 -ml-5 mb-3 border-l-4 border-blue-light pl-4">
     <a href="{{ $project->path() }}" class="text-black no-underline">{{ $project->title }}</a>
   </h3>
-  <div class="text-grey mb-4">{{ str_limit($project->description, 100) }}</div>
+  <div class="text-gray-400 lg:break-words">{{ str_limit($project->description, 100) }}</div>
   <footer>
     <form method="POST" action="{{ $project->path() }}" class="text-right">
       @method('DELETE')
       @csrf
-      <button type="submit" class="text-xs">Delete</button>
+      <button type="submit" class="ml-2 inline-block rounded-lg text-xs font-medium leading-none py-2 px-3 focus:outline-none bg-indigo-50 text-indigo-700 no-underline">Delete</button>
     </form>
   </footer>
 </div>
